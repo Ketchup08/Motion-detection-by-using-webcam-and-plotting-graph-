76626a2471bf46911753705b3978be17611bf440
Ketchup08 <89243204+Ketchup08@users.noreply.github.com>
2022-02-06 10:54:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from plot import df
+from bokeh.plotting import figure,show,output_file
+from bokeh.models import HoverTool,ColumnDataSource
+
+df["Start_string"] = df["Start"].dt.strftime('%Y-%m-%d %H:%M:%S')
+df["End_string"] = df["End"].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+cds=ColumnDataSource(df)
+
+p=figure(x_axis_type="datetime",height=500, width=500, title="Motion Graph ")
+p.yaxis.minor_tick_line_color=None
+p.yaxis.ticker.desired_num_ticks=1
+
+hover=HoverTool(tooltips=[("Star","@Start_stingq"),("End","@End")])
+p.add_tools(hover)
+q=p.quad(bottom=0, top=1, left="Start", right="End" , source=cds)
+
+Output_file=("graph.html")
+
+show(p)
